748f2d061a1a4836988c26484b23dda8f3331ee0
ujon <dkcushow@gmail.com>
2023-08-27 20:58:25 +0900

    init:-_-

---
+++ src/test/kotlin/io/ujon/server/UjonServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.ujon.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UjonServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
