6b6b622460bcb4a4b7868ffd38f50ec5ba501210
ujon <ujon94@gmail.com>
2024-09-03 00:09:34 -0700

    feat: delete docker infra configuration.
    The infrastructure configuration has been extracted into a repository named 'ujon-infra.

--- db/constant/role.sql
+++
@@ -1,4 +0,0 @@
-insert into ujon.tbl_role (role_id, name)
-values (uuid7(), 'master'),
-       (uuid7(), 'administrator'),
-       (uuid7(), 'user');
--- scripts/infra_local_up.sh
+++ scripts/infra_local_up.sh
@@ -3,3 +3,4 @@
 
 docker-compose -p ujon -f ./docker-compose.yml down
 docker-compose -p ujon -f ./docker-compose.yml up -d
+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
