c32a08f55012bb5943356969629cf19ff05bccd7
ujon <ujon94@gmail.com>
2024-11-09 18:33:58 -0800

    fix: @Repository missing

--- infra/ujon-postgesql/src/main/kotlin/io/ujon/infra/role/repository/querydsl/RoleQueryDslRepositoryImpl.kt
+++ infra/ujon-postgesql/src/main/kotlin/io/ujon/infra/role/repository/querydsl/RoleQueryDslRepositoryImpl.kt
@@ -1,11 +1,12 @@
 package io.ujon.infra.role.repository.querydsl
 
-import com.querydsl.jpa.JPAExpressions.selectFrom
 import io.ujon.infra.role.entity.QRole.Companion.role
 import io.ujon.infra.role.entity.Role
 import io.ujon.infra.role.repository.RoleQueryDslRepository
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
+import org.springframework.stereotype.Repository
 
+@Repository
 class RoleQueryDslRepositoryImpl : QuerydslRepositorySupport(Role::class.java), RoleQueryDslRepository {
     override fun default(): Role = from(role)
         .where(role.name.eq("user"))
